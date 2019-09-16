9d9a162ae54667d93f65bd979e85999cb114576e
GrigoriyGolikov <gregor@inbox.ru>
2019-09-16 09:54:21 +0300

    правки

--- lesson1/lesson1.php
+++ lesson1/lesson1.php
@@ -1,6 +1,7 @@
 <?php
 
-class Car{
+class Car
+{
     public $brand;
     public $color;
     public $power;
@@ -8,12 +9,13 @@ class Car{
     public $tankVolume;
     public $driver;
 
-    public function __construct($brand = "", $color = "", $power = "",$fuelConsumption = 0, $tankVolume = 0, $driver = "") {
-        $this -> brand = $brand;
-        $this -> color = $color;
-        $this -> power = $power;
-        $this -> fuelConsumption = $fuelConsumption;
-        $this -> tankVolume = $tankVolume;
+    public function __construct($brand = null, $color = null, $power = null,$fuelConsumption = 0, $tankVolume = 0, $driver = null) {
+        $this->brand = $brand;
+        $this->color = $color;
+        $this->power = $power;
+        $this->fuelConsumption = $fuelConsumption;
+        $this->tankVolume = $tankVolume;
+        $this->driver = $driver;
     }
 
     public function quantityOfKilometers($volume){
@@ -21,19 +23,20 @@ class Car{
     }
 
     public function setDriver($driver){
-        $this -> driver = $driver;
+        $this->driver = $driver;
     }
 
 }
 
-class Truck extends Car{
+class Truck extends Car
+{
     private $maxTonnage; // Максимальная загрузка кузова
     private $cargoWeight; // текущая загрузка кузова
 
-    public function __construct($brand = "", $color = "", $power = "",$fuelConsumption = 0, $tankVolume = 0,$maxTonnage = 0,$cargoWeight = 0) {
-        parent::__construct($brand, $color, $power,$fuelConsumption, $tankVolume);
-        $this -> maxTonnage = $maxTonnage;
-        $this -> cargoWeight = $cargoWeight;
+    public function __construct($brand = null, $color = null, $power = null,$fuelConsumption = 0, $tankVolume = 0, $driver = null,$maxTonnage = 0,$cargoWeight = 0) {
+        parent::__construct($brand, $color, $power,$fuelConsumption, $tankVolume, $driver);
+        $this->maxTonnage = $maxTonnage;
+        $this->cargoWeight = $cargoWeight;
     }
 
     public function remainingSpace($volume){
@@ -76,7 +79,8 @@ $car1->addLoad(60);
 
 
 
-class A {
+class A
+{
     public function foo() {
         static $x = 0;
         echo ++$x;
@@ -92,7 +96,8 @@ $a2->foo(); // Выведет 4
 
 
 echo '<br>';
-class A1 {
+class A1
+{
     public function foo() {
         static $x = 0;
         echo ++$x;
@@ -113,13 +118,15 @@ echo '<br>';
 
 
 // отработает аналогично предыдущему пункту. При создани класса нет скобок (). Их можно упустить если у конструктора класса нет аргументов.
-class A2 {
+class A2
+{
     public function foo() {
         static $x = 0;
         echo ++$x;
     }
 }
-class B2 extends A2 {
+class B2 extends A2
+{
 }
 $a1 = new A2;
 $b1 = new B2;
