15ca8ebaad709d580f200f14117eb7f91fec2e1f
GrigoriyGolikov <gregor@inbox.ru>
2019-09-23 11:28:52 +0300

    lesson4

--- lesson4/views/user.php
+++ lesson4/views/user.php
@@ -5,6 +5,6 @@
 ?>
 <form >
     <h1><?=$user->login?></h1>
-    <input type="password" maxlength="25" size="40" name="password"></p>
+    <input type="password" maxlength="25" size="40" name="password" value="<?=$user->pass?>"></p>
 </form>
 
